e13861f313663e106a3c5b1b49994ade8cb02ba8
Vina Melody <vina_melody@yahoo.com>
2022-07-23 21:21:38 +0800

    Setup tailwindui

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      },
+    },
   },
   plugins: [],
 }
