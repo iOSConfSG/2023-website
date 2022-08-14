cbc232d9aa9c0503f3b7fa16f5b6edaabc9c84d1
Vina Melody <vina_melody@yahoo.com>
2022-08-15 00:06:50 +0800

    Fix Tickets

--- src/components/layout/RootLayout.js
+++ src/components/layout/RootLayout.js
@@ -6,7 +6,8 @@ import {
   Slideshow,
   Speakers,
   Sponsors,
-  Tickets
+  Tickets,
+  Schedule,
 } from "@/components"
 import Image from "next/image"
 import Script from "next/script"
@@ -115,24 +116,8 @@ export default function RootLayout() {
         <Sponsors />
 
         {/* Stats section */}
-        <div className="relative bg-gray-900">
-          <div className="h-80 absolute inset-x-0 bottom-0 xl:top-0 xl:h-full">
-            <div className="h-full w-full xl:grid xl:grid-cols-2">
-              <div className="h-full xl:relative xl:col-start-2">
-                <img
-                  className="h-full w-full object-cover opacity-25 xl:absolute xl:inset-0"
-                  src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
-                  alt="People working on laptops"
-                />
-                <div
-                  aria-hidden="true"
-                  className="absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-gray-900 xl:inset-y-0 xl:left-0 xl:h-full xl:w-32 xl:bg-gradient-to-r"
-                />
-              </div>
-            </div>
-          </div>
-          <Tickets />
-        </div>
+        <Tickets />
+        <Schedule />
       </main>
       <Footer />
     </div>
