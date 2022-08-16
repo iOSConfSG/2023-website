58b19862bb871083af5e4d20230ed5e5f579cd95
Vina Melody <vina_melody@yahoo.com>
2022-08-16 22:56:06 +0800

    Add other pages

--- src/components/section/Sponsors.js
+++ src/components/section/Sponsors.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 export default function SponsorSection () {
   return (
-    <div className="bg-white">
+    <div id="sponsors" className="bg-white">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <p className="text-center text-4xl font-semibold text-black">
           Gold Sponsors
