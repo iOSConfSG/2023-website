f68e83b692fd266860deca6d0b8731b15fb5e8f5
Vina Melody <vina_melody@yahoo.com>
2022-08-20 11:17:24 +0800

    Faq

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,4 @@
 export { default as SpeakersData } from "./speakers"
 export { default as SponsorsData } from "./sponsors"
+export { default as FaqData } from "./faq"
 export { default as FooterNavigationData } from "./footerNavigation"
