c4ffdbd7846c18dc020135ebaecf26de51507764
Vina Melody <vina_melody@yahoo.com>
2022-10-03 16:23:04 +0800

    Undo redirect
    It does not work outside localhost

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
